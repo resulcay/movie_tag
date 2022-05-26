466fdd857f5d36c9e77748e86f21daea98806432
Resül Çay <resulcayop@gmail.com>
2022-05-26 18:39:48 +0300

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/movie_tag/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.movie_tag
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        backgroundColor: Colors.deepPurple,
+        appBar: AppBar(
+          backgroundColor: Colors.black45,
+          centerTitle: true,
+          title: const Text('Movie of the Year'),
+        ),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              const Text(
+                "The Avatar",
+                style: TextStyle(
+                    fontSize: 40,
+                    color: Colors.white70,
+                    fontWeight: FontWeight.bold),
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              Image.asset(
+                'images/avatar.png',
+                scale: 1,
+              )
+            ],
+          ),
+        ),
+      ),
+    ),
+  );
+}
